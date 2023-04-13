12919b8fd3cf6c42b30c5026be7cd1605a486c4d
eugenioenko <eyakhnenko@underline.com>
2023-04-13 13:35:31 -0700

    Refactored move strut

--- pkg/board.go
+++ pkg/board.go
@@ -190,11 +190,11 @@ func (board *Board) IsSquareAtHFile(square byte) bool {
 
 func (board *Board) AddQuiteOrCapture(from, to byte) bool {
 	if board.IsSquareEmpty(to) {
-		board.AddMove(NewMove(from, to, MoveQuiet))
+		board.AddMove(NewMove(from, to, MoveQuiet, [2]byte{0, 0}))
 		return true
 	} else {
 		if board.WhiteToMove && board.IsPieceAtSquareBlack(to) {
-			board.AddCapture(NewMove(from, to, MoveCapture))
+			board.AddCapture(NewMove(from, to, MoveCapture, [2]byte{0, 0}))
 		}
 		return false
 	}
@@ -250,165 +250,6 @@ func (board *Board) GeneratePiecesLocations() {
 			board.Pieces.Black.Queens = append(board.Pieces.Black.Queens, byte(index))
 		case piece == BlackKing:
 			board.Pieces.Black.King = byte(index)
-		}
-	}
-}
-
-func (board *Board) GeneratePawnMoves() {
-	squares := board.Pieces.White.Pawns
-	if !board.WhiteToMove {
-		squares = board.Pieces.Black.Pawns
-	}
-
-	for _, square := range squares {
-		// two squares forward
-		var leftRankSquare byte = SquareA2
-		if !board.WhiteToMove {
-			leftRankSquare = SquareA7
-		}
-		if square >= leftRankSquare && square <= leftRankSquare+8 {
-			var amountToMove int8 = 16
-			if !board.WhiteToMove {
-				amountToMove = -16
-			}
-			squareToMove := square - byte(amountToMove)
-			if board.IsSquareEmpty(squareToMove) {
-				board.AddMove(NewMove(square, squareToMove, MoveQuiet))
-			}
-		}
-
-		// one move forward and promotion
-		var amountToMove int8 = 8
-		var dirToMove int8 = 1
-		if !board.WhiteToMove {
-			dirToMove = -1
-		}
-		squareToMove := square - byte(amountToMove*dirToMove)
-		if board.IsSquareEmpty(squareToMove) {
-			if (board.WhiteToMove && board.IsSquareAt8thRank(squareToMove)) || (!board.WhiteToMove && board.IsSquareAt1stRank(squareToMove)) {
-				board.AddMove(NewMove(square, squareToMove, MovePromotion))
-			} else {
-				board.AddMove(NewMove(square, squareToMove, MoveQuiet))
-			}
-		}
-
-		// captures
-		if board.WhiteToMove {
-			leftSquare := square - 8 - 1
-			if !board.IsSquareAtHFile(square) && board.IsSquareOccupied(leftSquare) && board.IsPieceAtSquareBlack(leftSquare) {
-				if board.IsSquareAt8thRank(leftSquare) {
-					board.AddPromotion(NewMove(square, leftSquare, MovePromotionCapture))
-				} else {
-					board.AddCapture(NewMove(square, leftSquare, MoveCapture))
-				}
-			}
-			if !board.IsSquareAtHFile(square) && board.IsSquareOnPassant(leftSquare) && board.IsPieceAtSquareBlack(leftSquare+8) {
-				board.AddCapture(NewMove(square, leftSquare, MoveOnPassant))
-			}
-			rightSquare := square - 8 + 1
-			if !board.IsSquareAtAFile(square) && board.IsSquareOccupied(rightSquare) && board.IsPieceAtSquareBlack(rightSquare) {
-				if board.IsSquareAt1stRank(leftSquare) {
-					board.AddPromotion(NewMove(square, rightSquare, MovePromotionCapture))
-				} else {
-					board.AddCapture(NewMove(square, rightSquare, MoveCapture))
-				}
-			}
-			if !board.IsSquareAtAFile(square) && board.IsSquareOnPassant(rightSquare) && board.IsPieceAtSquareBlack(rightSquare+8) {
-				board.AddCapture(NewMove(square, rightSquare, MoveOnPassant))
-			}
-		} else {
-			rightSquare := square + 8 - 1
-			if !board.IsSquareAtHFile(square) && board.IsSquareOccupied(rightSquare) && board.IsPieceAtSquareBlack(rightSquare) {
-				if board.IsSquareAt1stRank(rightSquare) {
-					board.AddCapture(NewMove(square, rightSquare, MovePromotionCapture))
-				} else {
-					board.AddCapture(NewMove(square, rightSquare, MoveCapture))
-				}
-			}
-			if !board.IsSquareAtHFile(square) && board.IsSquareOnPassant(rightSquare) && board.IsPieceAtSquareBlack(rightSquare-8) {
-				board.AddCapture(NewMove(square, rightSquare, MoveOnPassant))
-			}
-			leftSquare := square + 8 + 1
-			if !board.IsSquareAtAFile(square) && board.IsSquareOccupied(leftSquare) && board.IsPieceAtSquareBlack(leftSquare) {
-				if board.IsSquareAt1stRank(rightSquare) {
-					board.AddCapture(NewMove(square, leftSquare, MovePromotionCapture))
-				} else {
-					board.AddCapture(NewMove(square, leftSquare, MoveCapture))
-				}
-			}
-			if !board.IsSquareAtHFile(square) && board.IsSquareOnPassant(rightSquare) && board.IsPieceAtSquareBlack(rightSquare-8) {
-				board.AddCapture(NewMove(square, rightSquare, MoveOnPassant))
-			}
-		}
-	}
-}
-
-func (board *Board) GenerateSlidingMoves(pieces []byte, startDir byte, endDir byte) {
-	for _, square := range pieces {
-		for dirOffset := startDir; dirOffset < endDir; dirOffset++ {
-			offset := BoardDirOffsets[dirOffset]
-			amountToMove := int8(SquaresToEdge[square][dirOffset])
-			for moveIndex := int8(1); moveIndex <= amountToMove; moveIndex++ {
-				squareTo := int8(square) + (offset * moveIndex)
-				isQuiteMove := board.AddQuiteOrCapture(square, byte(squareTo))
-				if !isQuiteMove {
-					break
-				}
-			}
-		}
-	}
-}
-
-func (board *Board) GenerateKingMoves() {
-	square := board.Pieces.White.King
-	if !board.WhiteToMove {
-		square = board.Pieces.Black.King
-	}
-	for dirOffset := 0; dirOffset < 8; dirOffset++ {
-		offset := BoardDirOffsets[dirOffset]
-		amountToMove := int8(SquaresToEdge[square][dirOffset])
-		if amountToMove > 0 {
-			squareTo := int8(square) + offset
-			board.AddQuiteOrCapture(square, byte(squareTo))
-		}
-	}
-}
-
-func (board *Board) GenerateRookMoves() {
-	rooks := board.Pieces.White.Rooks
-	if !board.WhiteToMove {
-		rooks = board.Pieces.Black.Rooks
-	}
-	board.GenerateSlidingMoves(rooks, 0, 4)
-}
-
-func (board *Board) GenerateBishopMoves() {
-	bishops := board.Pieces.White.Bishops
-	if !board.WhiteToMove {
-		bishops = board.Pieces.Black.Bishops
-	}
-	board.GenerateSlidingMoves(bishops, 4, 8)
-}
-
-func (board *Board) GenerateQueenMoves() {
-	queues := board.Pieces.White.Queens
-	if !board.WhiteToMove {
-		queues = board.Pieces.Black.Queens
-	}
-	board.GenerateSlidingMoves(queues, 0, 8)
-}
-
-func (board *Board) GenerateKnightMoves() {
-	knights := board.Pieces.White.Knights
-	if !board.WhiteToMove {
-		knights = board.Pieces.Black.Knights
-	}
-	for _, square := range knights {
-		for moveIndex := 0; moveIndex < 8; moveIndex++ {
-			squareTo := SquareKnightJumps[square][moveIndex]
-			if squareTo < 255 {
-				board.AddQuiteOrCapture(square, squareTo)
-			}
 		}
 	}
 }
